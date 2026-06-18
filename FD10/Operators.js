2eeb4487418b7364dabe752e0b1bfebdca4722e0
Abhinandan Singh <abhisakhhusingh@gmail.com>
2026-06-18 11:11:17 +0530

    AAj maine data type and operators padha

---
+++ FD10/Operators.js
@@ -0,0 +1,50 @@
+//Arithmetic Op -> + ,- ,* ,% ,-- ,++,**, /
+// + (Addition)
+// a=9
+// b=3,c=10
+// console.log(a+b+c);
+
+
+// + (Subtraction)
+// a=9
+// b=3,c=10
+// console.log(a-b-c);
+
+// * Multiplication
+// a=9
+// b=3
+// console.log(a*b);
+
+// / Devision
+// a=9
+// b=3
+// console.log(a/b);
+
+// % module(Reminder)
+// a=14
+// b=3
+// rem=a%b
+// console.log(rem);
+
+// ** exponential
+// a=7
+// b=2
+// exp=a**b
+// console.log(exp);
+
+
+// Increment (++) decrement(--)
+// num1=45
+// console.log(num1++)
+// console.log(num1);
+
+num2=11
+console.log(num2--)
+console.log(num2--)
+console.log(num2--)
+console.log(num2);
+
+
+
+
+
